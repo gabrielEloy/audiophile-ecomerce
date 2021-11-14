bfa26c3804d82903b7158eae62bc72fa49dded87
Gabriel Eloy <gabrieleloy@Gabriels-MacBook-Pro.local>
2021-11-14 10:02:20 -0300

    ➕styled-components

---
+++ styles/globalStyle.tsx
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+
+const GlobalStyle = createGlobalStyle`
+    h1{
+        font-family: 'Manrope', sans-serif;
+    }
+`
+
+
+export default GlobalStyle;
